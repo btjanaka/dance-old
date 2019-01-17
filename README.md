839cb6ef9304a22f9872bcc32de99b15b0e6e528
btjanaka <bryon.tjanaka@gmail.com>
2019-01-16 23:50:37 -0800

    Refactor dance.py and improve filtering
    This commit accomplishes two things. First, it refactors the dance.py
    script to use a class in order to better manage the program state.
    Thanks to this class, there is no longer a need to pass around various
    lists and strings. Second, this commit improves the filtering of
    molecules from the database. It selects only molecules with just one
    trivalent nitrogen in them and calculates the Wiberg bond order on each.
    In the future, molecules will be split into bins based on bond order and
    then sorted by size. The smallest molecules in each bin will then be
    chosen as the final set of molecules.

--- README.md
+++ README.md
@@ -1,40 +1,77 @@
 # DANCE - DAtabase of Nitrogen CEnters
 
-This tool filters out trivalent nitrogen molecules from the eMolecules database.
-The molecules are passed in as mol2 files, and a file with SMILES strings
-representing the trivalent nitrogen molecules is outputted.
+<!-- toc -->
+
+- [Usage](#usage)
+  * [Example](#example)
+  * [As a Library](#as-a-library)
+- [A Note on Logging](#a-note-on-logging)
+
+<!-- tocstop -->
+
+This tool filters molecules passed in as directories consisting of mol2 files.
+The molecules are selected in several steps:
+1. Only molecules with a single trivalent/invertible nitrogen are selected
+2. Those molecules are sorted by Wiberg bond order and separated into bins
+3. In each bin, the smallest molecules are chosen
+Thus, the final set of molecules is "small molecules with a single trivalent
+nitrogen, with a wide range of Wiberg bond orders". These molecules are stored
+as SMILES strings. Ultimately, this tool is used for filtering molecules from
+various databases.
+
+*Note: So far only step 1 is fully implemented. Step 2 is partially implemented,
+but we are still working on determining the bins. Step 3 has yet to be
+implemented.*
 
 ## Usage
 Below is the help message for dance.py. Adding `python` before the
 invocation of dance.py is optional.
 ```
-usage: dance.py [-h] [-o OUTPUT] DIRS [DIRS ...]
+usage: dance.py [-h] [--mol2dirs MOL2DIRS] [--output FILENAME.smi]
+                [--log LEVEL]
 
-Finds molecules with trivalent nitrogens in a directory and stores them as
-SMILES strings.
+Filters molecules stored in mol2 files from a list of directories and stores
+them as SMILES strings
 
 optional arguments:
   -h, --help            show this help message and exit
-  -o OUTPUT, --output OUTPUT
-                        the name of an output file for the SMILES strings -
-                        defaults to output.smi
-
-required arguments:
-  DIRS                  directories with mol2 files to be filtered
+  --mol2dirs MOL2DIRS   a comma-separated list of directories with mol2 files
+                        to be filtered (default: )
+  --output FILENAME.smi
+                        location of output file (default: output.smi)
+  --log LEVEL           logging level - one of DEBUG, INFO, WARNING, ERROR,
+                        and CRITICAL - See
+                        https://docs.python.org/3/howto/logging.html for more
+                        information (default: info)
 ```
 
 ### Example
 ```
-dance.py -o output.smi ./database1 ./database2 ./database3
+dance.py --mol2dirs dir1,dir2,dir3 --output ouput.smi --log debug
 ```
-Writes SMILES representations of the molecules with trivalent nitrogens in
-database1, database2, and database3 to output.smi.
+Reads in molecules from dir1, dir2, and dir3, filters them, and writes the
+results to output.smi. Log messages as low as DEBUG are printed to stderr.
 
-## Other Notes
-Status messages printed during the program are printed to stderr to avoid
-interfering with any real output, so if you wish to capture them, you will have
-to capture stderr. For example, the following command will redirect the status
-messages to a file called status.txt.
+### As a Library
+It is possible to use dance.py as a library. Simply import the module,
+construct a DanceMoleculeFilter object, call the `.generate()` method, and you
+will have filtered the appropriate molecules and stored them in a file.
+```Python
+import dance
+
+mol2dirs = ["dir1", "dir2"]
+output = "output.smi"
+
+dance_filter = dance.DanceMoleculeFilter(mol2dirs, output)
+dance_filter.generate()
 ```
-dance.py dev 2> status.txt
+
+## A Note on Logging
+Python's standard logging library is used to write log messages of varying
+severity to stderr. The severity level required for a message to be printed can
+be adjusted with the `--log` flag. To capture the messages in a file, you will
+have to redirect stderr to a file. For example, the following command will
+redirect stderr to a file called status.txt when running dance.py.
+```
+dance.py --mol2dirs dir1,dir2,dir3 2> status.txt
 ```
--- dance.py
+++ dance.py
@@ -1,93 +1,216 @@
 #!/usr/bin/env python3
 # Author: Bryon Tjanaka
-# Purpose: Filters mol2 files in a list of directories to find molecules with
-#          trivalent nitrogens within them. Stores these filtered molecules as
-#          SMILES strings.
-# Usage: python databasen.py [-h] [-o OUTPUT] DIRS [DIRS ...]
+# See README for more information.
 
 import argparse
 import glob
+import logging
 import sys
 from openeye import oechem
+from openeye import oequacpac
 
 
-def print_status(status: str):
-    """Prints a short status message to stderr."""
-    print(status, file=sys.stderr)
-
-
-def parse_commandline_flags() -> (str, [str]):
+def parse_commandline_flags() -> {}:
     """Uses argparse to handle all parsing of command line flags.
 
     Returns:
-        name of the output file to which to write the SMILES
-        strings
+        a dictionary holding the values of all the flags
     Raises:
         IndexError: not enough arguments were passed in
     """
     parser = argparse.ArgumentParser(
-        description=
-        "Finds molecules with trivalent nitrogens in a directory and stores them as SMILES strings."
-    )
+        description=(
+            "Filters molecules stored in mol2 files from a list of directories "
+            "and stores them as SMILES strings"),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
     parser.add_argument(
-        "-o",
-        "--output",
+        "--mol2dirs",
+        default="",
         help=
-        "the name of an output file for the SMILES strings - defaults to output.smi"
-    )
-    parser.set_defaults(output="output.smi")
+        "a comma-separated list of directories with mol2 files to be filtered")
+    parser.add_argument(
+        "--output",
+        default="output.smi",
+        metavar="FILENAME.smi",
+        help="location of output file")
+    parser.add_argument(
+        "--log",
+        default="info",
+        metavar="LEVEL",
+        help=("logging level - one of DEBUG, INFO, WARNING, ERROR, and CRITICAL"
+              " - See https://docs.python.org/3/howto/logging.html for more "
+              "information"))
 
-    req_args = parser.add_argument_group("required arguments")
-    req_args.add_argument(
-        "DIRS", nargs='+', help="directories with mol2 files to be filtered")
+    args = vars(parser.parse_args())
+    for comma_sep_list in ["mol2dirs"]:
+        comma_sep_list = comma_sep_list.strip()
+        args[comma_sep_list] = [] if args[comma_sep_list] == "" else args[
+            comma_sep_list].split(",")
+    return args
 
-    args = parser.parse_args()
 
-    return args.output, args.DIRS
+def configure_logging(loglevel: str):
+    """Configure Python's logging library"""
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f"Invalid log level: {loglevel}")
+    logging.basicConfig(
+        format="%(levelname)s: %(message)s", level=numeric_level)
 
 
-def generate_mol2files(input_dirs: [str]) -> [str]:
-    """Generator that yields mol2 files from the given input directories.
-
-    A generator has to be used to store as few items as possible. We know that
-    the dataset can have millions of files.
+class DanceMoleculeFilter:
     """
-    for d in input_dirs:
-        for f in glob.iglob(d + "/*.mol2"):
-            yield f
+    DanceMoleculeFilter is a class for performing all the various filtering
+    actions. To use, instantiate it with the list of mol2dirs and output. Then
+    call the generate() method to filter the molecules and write them to a file
+    as SMILES strings.
 
-
-def check_one_molecule(mol2file: str) -> oechem.OEGraphMol or None:
-    """Checks if the molecule in the given file has a trivalent nitrogen.
-
-    Returns:
-        The molecule itself if any of the atoms are trivalent nitrogens,
-        otherwise None.
+    Attributes:
+        _mol2dirs: list of names of directories with mol2 files
+        _output: the name of the output file to which to write the molecules
+        _mols: a list storing the molecules the class is currently handling
     """
-    in_stream = oechem.oemolistream(mol2file)
-    in_stream.SetFormat(oechem.OEFormat_MOL2)
-    mol = oechem.OEGraphMol()
-    oechem.OEReadMolecule(in_stream, mol)
-    is_trivalent_n = oechem.OEIsInvertibleNitrogen()
-    return mol if any(is_trivalent_n(atom) for atom in mol.GetAtoms()) else None
 
+    #
+    # static constants
+    #
 
-def generate_output_file(out_filename: str, input_dirs: [str]):
-    """Performs the main workflow for filtering out the molecules."""
-    out_stream = oechem.oemolostream(out_filename)
-    out_stream.SetFormat(oechem.OEFormat_USM)
+    # Name of the label used for storing total Wiberg bond order in molecules
+    WIBERG_MOL = "TotalWibergBondOrder"
 
-    for mol2file in generate_mol2files(input_dirs):
-        checked_mol = check_one_molecule(mol2file)
-        if checked_mol is not None:
-            oechem.OEWriteMolecule(out_stream, checked_mol)
+    # Name of the label used for storing Wiberg bond order in inidividual bonds
+    WIBERG_BOND = "WibergBondOrder"
 
-    out_stream.close()
+    # Trivalent Nitrogen checker - instead of repeatedly constructing this class
+    IS_TRI_N = oechem.OEIsInvertibleNitrogen()
+
+    #
+    # Public
+    #
+
+    def __init__(self, mol2dirs: [str], output: str):
+        self._mol2dirs = mol2dirs
+        self._output = output
+        self._mols = []
+
+    def generate(self):
+        """Pushes all the molecules through the various filtering steps."""
+        logging.info("STARTING FILTERING")
+        self._filter_tri_n()
+        self._apply_wiberg()
+        self._write_to_smiles_file()
+        logging.info("FINISHED FILTERING")
+
+    #
+    # Private
+    #
+
+    def _filter_tri_n(self):
+        """
+        Filters out the molecules with one trivalent nitrogen from the molecules
+        stored as mol2 files.
+        """
+        logging.info(f"Filtering trivalent nitrogen molecules "
+                     f"in directories {self._mol2dirs}")
+
+        for mol2file in self._generate_mol2files():
+            mol, ok = self._check_one_molecule(mol2file)
+            logging.debug(f"tri-n-check: molecule {mol2file}: {ok}")
+            if ok: self._mols.append(mol)
+
+    def _apply_wiberg(self):
+        """
+        Adds Wiberg bond order annotations and sorts the molecules by them.
+        """
+        logging.info("Annotating molecules with Wiberg bond order")
+
+        for mol in self._mols:
+            mol.SetData(self.WIBERG_MOL, self._calc_wiberg(mol))
+            logging.debug(f"wiberg-annotation: molecule {mol.GetTitle()}: "
+                          f"{mol.GetData(self.WIBERG_MOL)}")
+
+        self._mols.sort(key=lambda m: m.GetData(self.WIBERG_MOL))
+
+    def _write_to_smiles_file(self):
+        """Writes the molecules to the SMILES file"""
+        logging.info(f"Writing molecules to SMILES file {self._output}")
+        ostream = oechem.oemolostream(self._output)
+        ostream.SetFormat(oechem.OEFormat_USM)
+        for mol in self._mols:
+            oechem.OEWriteMolecule(ostream, mol)
+        ostream.close()
+
+    #
+    # Private (utility)
+    #
+
+    def _generate_mol2files(self) -> str:
+        """Generator that yields mol2 files from the input directories.
+
+        A generator is used to try to store as few items as possible. We know
+        that the dataset can have millions of files.
+        """
+        for mol2dir in self._mol2dirs:
+            for mol2file in glob.iglob(mol2dir + "/*.mol2"):
+                yield mol2file
+
+    def _check_one_molecule(self, mol2file: str) -> (oechem.OEMol, bool):
+        """Checks if the molecule in the given file has only one trivalent
+        nitrogen.
+
+        Returns:
+            The molecule itself, as well as a bool telling if there is only one
+            trivalent nitrogen.
+        """
+        istream = oechem.oemolistream(mol2file)
+        istream.SetFormat(oechem.OEFormat_MOL2)
+        mol = oechem.OEMol()
+        oechem.OEReadMolecule(istream, mol)
+
+        tri_n_count = sum(
+            1 if self.IS_TRI_N(atom) else 0 for atom in mol.GetAtoms())
+        return mol, tri_n_count == 1
+
+    def _calc_wiberg(self, mol: oechem.OEMol) -> float:
+        """
+        Calculates the sum of the Wiberg bond order among the bonds surrounding
+        the trivalent nitrogen in the molecule (there should be only one at this
+        point). Only considers the first conformation of the molecule.
+
+        Based on Victoria Lim's am1wib.py - see
+        https://github.com/vtlim/misc/blob/master/oechem/am1wib.py
+        """
+        total = 0.0
+        for conf in mol.GetConfs():
+            charged_copy = oechem.OEMol(mol)
+            # TODO: Deprecation warning here, switch to OEAssignCharges
+            status = oequacpac.OEAssignPartialCharges(
+                charged_copy, oequacpac.OECharges_AM1BCCSym, False, False)
+            if not status:
+                raise RuntimeError(
+                    f"OEAssignPartialCharges returned error code {status}")
+
+            # Copy over bonds and charges from the charged copy to our copy
+            for charged_atom, atom in zip(charged_copy.GetAtoms(),
+                                          mol.GetAtoms()):
+                atom.SetPartialCharge(charged_atom.GetPartialCharge())
+            for charged_bond, bond in zip(charged_copy.GetBonds(),
+                                          mol.GetBonds()):
+                bond.SetData(self.WIBERG_BOND,
+                             charged_bond.GetData(self.WIBERG_BOND))
+
+            # Sum up bond orders around the trivalent nitrogen
+            for atom in conf.GetAtoms(self.IS_TRI_N):
+                for bond in atom.GetBonds():
+                    total += bond.GetData(self.WIBERG_BOND)
+                break  # terminate after one trivalent nitrogen
+            break  # terminate after one conformation
+        return total
 
 
 if __name__ == "__main__":
-    out_filename, input_dirs = parse_commandline_flags()
-    print_status("Filtering molecules in directories " + str(input_dirs))
-    print_status("Writing to " + out_filename)
-    generate_output_file(out_filename, input_dirs)
-    print_status("Done")
+    args = parse_commandline_flags()
+    configure_logging(args["log"])
+    dance_filter = DanceMoleculeFilter(args["mol2dirs"], args["output"])
+    dance_filter.generate()
